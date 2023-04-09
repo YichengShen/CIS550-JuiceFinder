a9645be4151cdf043bd7fbcbc39eb1396ede8dc8
lizyn <26119970+lizyn@users.noreply.github.com>
2023-04-09 15:49:35 -0400

    Create tests.yml
    Whenever code changes happen in the client or server, they will trigger tests and build of the corresponding part

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  // console.log("trigger client tests. Can safely delete this line later.");
   return (
     <div className="App">
       <header className="App-header">
