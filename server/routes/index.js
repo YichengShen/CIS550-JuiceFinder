7daca16e8e8acbf81bfb5f63b6e57fe4ee3289c5
lewei22 <lewei@seas.upenn.edu>
2023-04-09 16:52:30 -0400

    Add /stats to router

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.use("/vehicles", require("./vehicles"));
 router.use("/adaptors", require("./adapters"));
 router.use("/stations", require("./stations"));
+router.use("/stats", require("./stats"));
 
 module.exports = router;
