0b6af279989668e1b36e46e99dd2c7a66ff24412
YichengShen <jsszsyc@hotmail.com>
2023-04-10 09:12:17 -0400

    Add react router and firebase

---
+++ client/src/auth/AuthContext.js
@@ -0,0 +1,67 @@
+import React, { useContext, useState, useEffect, useMemo } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail,
+  onAuthStateChanged,
+} from "firebase/auth";
+import PropTypes from "prop-types";
+import { auth } from "./Firebase";
+
+const AuthContext = React.createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const signup = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logout = () => {
+    return signOut(auth);
+  };
+
+  const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = useMemo(
+    () => ({
+      currentUser,
+      login,
+      signup,
+      logout,
+      resetPassword,
+    }),
+    [currentUser, login, signup, logout, resetPassword]
+  );
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
