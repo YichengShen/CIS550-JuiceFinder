4d02018fd2cad08709fe5647d2832a39e2320c7f
YichengShen <jsszsyc@hotmail.com>
2023-04-10 17:19:25 -0400

    Add checkAuth middleware

--- server/services/firebaseAdmin.js
+++ server/services/firebaseAdmin.js
@@ -19,6 +19,8 @@ const serviceAccount = {
   client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
 };
 
+console.log(serviceAccount);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
