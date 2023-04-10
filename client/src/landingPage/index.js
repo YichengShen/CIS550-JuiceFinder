6da538e4106b300921e53940b317062521e3b523
lizyn <26119970+lizyn@users.noreply.github.com>
2023-04-10 16:26:33 -0400

    Prevent Firebase Analytics initialization in unsupported environments

--- client/src/landingPage/index.js
+++ client/src/landingPage/index.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
-import { logEvent } from "firebase/analytics";
+import { logEvent, isSupported } from "firebase/analytics";
 import { analytics } from "../auth/Firebase";
 
 function Landing() {
   useEffect(() => {
     // Log a page_view event when the HomePage component is mounted
-    logEvent(analytics, "page_view", { page_title: "HomePage" });
+    isSupported().then((yes) =>
+      yes
+        ? logEvent(analytics, "page_view", { page_title: "HomePage" })
+        : () => {}
+    );
   }, []);
   return <div>landing page</div>;
 }
