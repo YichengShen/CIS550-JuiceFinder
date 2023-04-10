bbc63f08f26ae0b190a74860f3b70efb13e1aed0
YichengShen <jsszsyc@hotmail.com>
2023-04-10 09:12:17 -0400

    Add react router and firebase

--- client/src/landingPage/index.js
+++ client/src/landingPage/index.js
@@ -1,17 +1,13 @@
 import React, { useEffect } from "react";
-import { logEvent, isSupported } from "firebase/analytics";
+import { logEvent } from "firebase/analytics";
 import { analytics } from "../auth/Firebase";
 
 function Landing() {
   useEffect(() => {
     // Log a page_view event when the HomePage component is mounted
-    isSupported().then((yes) =>
-      yes
-        ? logEvent(analytics, "page_view", { page_title: "HomePage" })
-        : () => {}
-    );
+    logEvent(analytics, "page_view", { page_title: "HomePage" });
   }, []);
-  return <div>landing page</div>;
+  return <div>landing</div>;
 }
 
 export default Landing;
