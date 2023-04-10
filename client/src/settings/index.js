b11af5a55731a0983a1158ec4deb19dd3ea99309
YichengShen <jsszsyc@hotmail.com>
2023-04-10 16:05:34 -0400

    Add signup and login

--- client/src/settings/index.js
+++ client/src/settings/index.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../auth/AuthContext";
 
 function Settings() {
-  return <div>settings</div>;
+  const { currentUser, logout } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    setError("");
+    setLoading(true);
+    try {
+      await logout();
+      navigate("/login");
+    } catch (logoutError) {
+      setError(`Failed to log out. ${logoutError.message}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="settings">
+      <h1>Settings</h1>
+      {error && <div>{error}</div>}
+      <div>
+        <h3>Email:</h3>
+        <p>{currentUser.email}</p>
+      </div>
+      <div>
+        <h3>Creation Time:</h3>
+        <p>{new Date(currentUser.metadata.creationTime).toLocaleString()}</p>
+      </div>
+      <div>
+        <h3>User ID:</h3>
+        <p>{currentUser.uid}</p>
+      </div>
+      <button type="button" disabled={loading} onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
 }
 
 export default Settings;
