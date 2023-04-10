51b6e22b03cfe34805adaeec1aa365421608f0b3
YichengShen <jsszsyc@hotmail.com>
2023-04-10 16:05:34 -0400

    Add signup and login

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
 
 function Login() {
-  return <div>login</div>;
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setError("");
+    setLoading(true);
+
+    try {
+      await login(emailRef.current.value, passwordRef.current.value);
+      navigate("/settings"); // redirect to settings page upon login
+    } catch (loginError) {
+      setError(`Failed to log in. ${loginError.message}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      {error && <div>{error}</div>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">
+            Email:
+            <input type="email" id="email" ref={emailRef} required />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="password">
+            Password:
+            <input type="password" id="password" ref={passwordRef} required />
+          </label>
+        </div>
+        <button type="submit" disabled={loading}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
