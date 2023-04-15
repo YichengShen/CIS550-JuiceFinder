854ce66767d17e0cbf27b768fd635c6a2e1ae9b9
AdvisorChuanChuan <licha@shanghaitech.edu.cn>
2023-04-15 15:52:52 -0400

    Fix lint errors. Enable eslint autofix.

--- lint-staged-pre-commit.js
+++ lint-staged-pre-commit.js
@@ -1,6 +1,14 @@
 module.exports = {
-  "client/**/*.{js,jsx,ts,tsx,json,css}": ["prettier --config ./client/.prettierrc --write"],
-  "client/**/*.{js,jsx,ts,tsx}": ["eslint --config ./client/.eslintrc.json --max-warnings=0 --fix"],
-  "server/**/*.{js,jsx,ts,tsx,json,css}": ["prettier --config ./server/.prettierrc --write"],
-  "server/**/*.{js,jsx,ts,tsx}": ["eslint --config ./server/.eslintrc.json --max-warnings=0 --fix"],
+  "client/**/*.{js,jsx,ts,tsx,json,css}": [
+    "prettier --config ./client/.prettierrc --write",
+  ],
+  "client/**/*.{js,jsx,ts,tsx}": [
+    "eslint --config ./client/.eslintrc.json --max-warnings=0 --fix",
+  ],
+  "server/**/*.{js,jsx,ts,tsx,json,css}": [
+    "prettier --config ./server/.prettierrc --write",
+  ],
+  "server/**/*.{js,jsx,ts,tsx}": [
+    "eslint --config ./server/.eslintrc.json --max-warnings=0 --fix",
+  ],
 };
