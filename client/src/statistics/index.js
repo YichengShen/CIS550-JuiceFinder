44261c5eddc6fa0744044f62af730706a68b8659
lewei22 <lewei@seas.upenn.edu>
2023-04-16 18:57:44 -0400

    Testing stacked bar chart and fetching data from server

--- client/src/statistics/index.js
+++ client/src/statistics/index.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Card, Col, Row } from "antd";
+import { Column } from "@ant-design/plots";
 
-// import {
+import serverConfig from "../config.json";
+
+// import {c
 //   // Form,
 //   // FormInput,
 //   // FormGroup,
@@ -14,13 +17,83 @@ import { Button, Card, Col, Row } from "antd";
 
 import SelectComponent from "./SelectComponent";
 
+function groupedChart() {
+  const [data, setData] = useState([]);
+
+  const asyncFetch = () => {
+    fetch(
+      `http://${serverConfig.server_host}:${serverConfig.server_port}/stats/overview/afsByTypeState?stationType=electric`
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log("fetch data failed", error);
+      });
+  };
+
+  useEffect(() => {
+    asyncFetch();
+  }, []);
+
+  const config = {
+    data,
+    xField: "state",
+    yField: "numStations",
+    isGroup: true,
+    isStack: true,
+    seriesField: "stype",
+    columnStyle: {
+      radius: [20, 20, 0, 0],
+    },
+  };
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Column {...config} />;
+}
+
+function afsByTypeStateFig() {
+  const [data, setData] = useState([]);
+
+  const asyncFetch = () => {
+    fetch(
+      `http://${serverConfig.server_host}:${serverConfig.server_port}/stats/overview/afsByTypeState?stationType=All`
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log("fetch data failed", error);
+      });
+  };
+
+  useEffect(() => {
+    asyncFetch();
+  }, []);
+
+  const config = {
+    data,
+    xField: "state",
+    yField: "numStations",
+    isGroup: true,
+    isStack: false,
+    seriesField: "stype",
+    columnStyle: {
+      radius: [20, 20, 0, 0],
+    },
+  };
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Column {...config} />;
+}
+
 function Statistics() {
   return (
     <div>
       <h1>Statistics</h1>
       <Button type="primary">Primary Button</Button>
-      <Row gutter={16}>
-        <Col span={12}>
+      <Row gutter={16} type="flex">
+        <Col span={6}>
           <Card title="🏅 Title1">
             Paragraph 1
             <div>
@@ -33,6 +106,20 @@ function Statistics() {
                 "Select station type"
               )}
             </div>
+          </Card>
+        </Col>
+        <Col span={18}>
+          <Card>Content 1{afsByTypeStateFig()}</Card>
+        </Col>
+      </Row>
+
+      <Row gutter={16} type="flex">
+        <Col span={6}>
+          <Card title="🏅 Title2">
+            Paragraph 2
+            <div>
+              {SelectComponent("state", "Select state:", "Select state")}
+            </div>
             <div>
               {SelectComponent(
                 "vehicleType",
@@ -42,26 +129,37 @@ function Statistics() {
             </div>
           </Card>
         </Col>
-        <Col span={12}>
-          <Card>
-            {SelectComponent(
-              "vehicleType",
-              "Select light-duty vehicle type:",
-              "Select vehicle type"
-            )}
-          </Card>
+        <Col span={18}>
+          <Card>Content 2{groupedChart()}</Card>
         </Col>
       </Row>
 
-      <Row gutter={16}>
-        <Col span={24}>
-          <Card title="🏅 Title2" bordered={false}>
-            Paragraph 2
+      <Row gutter={16} type="flex">
+        <Col span={6}>
+          <Card title="🏅 Title3">
+            Paragraph 3
+            <div>
+              {SelectComponent("state", "Select state:", "Select state")}
+            </div>
             <div>
               {SelectComponent("port", "Select port:", "Select port_1")}
             </div>
             <div>
               {SelectComponent("port", "Select port:", "Select port_2")}
+            </div>
+          </Card>
+        </Col>
+        <Col span={18}>
+          <Card>Content 3{groupedChart()}</Card>
+        </Col>
+      </Row>
+
+      <Row gutter={16} type="flex">
+        <Col span={6}>
+          <Card title="🏅 Title4">
+            Paragraph 4
+            <div>
+              {SelectComponent("state", "Select state:", "Select state")}
             </div>
             <div>
               {SelectComponent(
@@ -77,6 +175,20 @@ function Statistics() {
                 "Select speed_2"
               )}
             </div>
+          </Card>
+        </Col>
+        <Col span={18}>
+          <Card>Content 4{groupedChart()}</Card>
+        </Col>
+      </Row>
+
+      <Row gutter={16} type="flex">
+        <Col span={6}>
+          <Card title="🏅 Title5">
+            Paragraph 5
+            <div>
+              {SelectComponent("state", "Select state:", "Select state")}
+            </div>
             <div>
               {SelectComponent(
                 "network",
@@ -90,6 +202,9 @@ function Statistics() {
               )}
             </div>
           </Card>
+        </Col>
+        <Col span={18}>
+          <Card>Content 5{groupedChart()}</Card>
         </Col>
       </Row>
     </div>
