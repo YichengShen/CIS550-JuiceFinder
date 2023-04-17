44584f129754246c7b089ee85cdd719893a2af6c
YichengShen <jsszsyc@hotmail.com>
2023-04-16 22:35:48 -0400

    Add theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PropTypes from "prop-types";
 import { AuthProvider } from "./auth/AuthContext";
 import WithAuthRedirect from "./common/WithAuthRedirect";
 import Landing from "./landingPage";
 import Login from "./auth/Login";
 import Signup from "./auth/Signup";
-import HomePage from "./home";
+import Map from "./map";
 import Statistics from "./statistics";
 import Settings from "./settings";
 
-function App() {
+function App({ toggleTheme }) {
+  // console.log("trigger client tests. Can safely delete this line later.");
   return (
     <Router>
       <AuthProvider>
         <Routes>
-          <Route exact path="/" element={<Landing />} />
+          <Route
+            exact
+            path="/"
+            element={<Landing toggleTheme={toggleTheme} />}
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<HomePage />} />
+          <Route path="/map" element={<Map />} />
           <Route path="/statistics" element={<Statistics />} />
           <Route
             path="/settings"
@@ -32,5 +38,9 @@ function App() {
     </Router>
   );
 }
+
+App.propTypes = {
+  toggleTheme: PropTypes.func.isRequired,
+};
 
 export default App;
--- client/src/landingPage/index.js
+++ client/src/landingPage/index.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
+import PropTypes from "prop-types";
 import { logEvent, isSupported } from "firebase/analytics";
+import { useTheme } from "@mui/material/styles";
+import { Typography, Button, Box } from "@mui/material";
 import { analytics } from "../auth/Firebase";
 
-function Landing() {
+function Landing({ toggleTheme }) {
+  const theme = useTheme();
   useEffect(() => {
     // Log a page_view event when the HomePage component is mounted
     isSupported().then((yes) =>
@@ -11,7 +15,29 @@ function Landing() {
         : () => {}
     );
   }, []);
-  return <div>landing page</div>;
+  return (
+    <Box
+      sx={{
+        backgroundColor: theme.palette.background.default,
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Typography variant="h1" sx={{ color: theme.palette.primary.main }}>
+        Landing Page
+      </Typography>
+      <Button variant="contained" color="secondary" onClick={toggleTheme}>
+        Click here to test Toggle Light/Dark Theme
+      </Button>
+    </Box>
+  );
 }
+
+Landing.propTypes = {
+  toggleTheme: PropTypes.func.isRequired,
+};
 
 export default Landing;
