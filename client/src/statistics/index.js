ec3e1ebda38368f2d7d00c2486e5da797cecf0cf
lewei22 <lewei@seas.upenn.edu>
2023-04-17 00:11:42 -0400

    Import Tabs/Typography from antd, new tabs menu with defaultActiveKey value

--- client/src/statistics/index.js
+++ client/src/statistics/index.js
@@ -1,67 +1,32 @@
-import React, { useState } from "react";
-import { Card } from "antd";
+import React from "react";
 
-// import React, { useState, useEffect } from "react";
-// import { Card, Col, Row } from "antd";
+import { Tabs, Typography } from "antd";
 
-// import {c
-//   // Form,
-//   // FormInput,
-//   // FormGroup,
-//   Button,
-//   Card,
-//   CardBody,
-//   CardTitle,
-//   // Container,
-// } from "shards-react";
-
+// import three js files for tab menu
 import OverviewTab from "./OverviewTab";
+
 import ElectricStationTab from "./ElectricStationTab";
+
 import EVFriendlinessTab from "./EVFriendlinessTab";
 
-// variable for tab list
-const tabListNoTitle = [
-  {
-    key: "Overview",
-    tab: "AFS Resources Overview",
-  },
-  {
-    key: "ElectricStation",
-    tab: "Electric Charging Stations",
-  },
-  {
-    key: "EVFriendliness",
-    tab: "EV Friendliness",
-  },
-];
-
-const contentListNoTitle = {
-  Overview: <OverviewTab />,
-  ElectricStation: <ElectricStationTab />,
-  EVFriendliness: <EVFriendlinessTab />,
-};
+const { TabPane } = Tabs;
+const { Title } = Typography;
 
 function Statistics() {
-  const [activeTabKey, setActiveTabKey] = useState("app");
-
-  const onTabChange = (key) => {
-    setActiveTabKey(key);
-  };
-
   return (
     <div>
-      <h1>Statistics</h1>
-      <Card
-        style={{
-          width: "100%",
-        }}
-        tabList={tabListNoTitle}
-        activeTabKey={activeTabKey}
-        // tabBarExtraContent={<a href="#">More</a>}
-        onTabChange={onTabChange}
-      >
-        {contentListNoTitle[activeTabKey]}
-      </Card>
+      <Title>Statistics</Title>
+      <Tabs defaultActiveKey="Overview">
+        <TabPane tab="AFS Resources Overview" key="Overview">
+          <OverviewTab />
+        </TabPane>
+        <TabPane tab="Electric Charging Stations" key="ElectricStation">
+          <ElectricStationTab />
+        </TabPane>
+        <TabPane tab="EVFriendliness" key="EVFriendliness">
+          <EVFriendlinessTab />
+        </TabPane>
+      </Tabs>
     </div>
   );
 }
