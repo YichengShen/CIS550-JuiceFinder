f50739a4687e999d27da0d403dc4167c5e785644
YichengShen <jsszsyc@hotmail.com>
2023-04-17 16:35:55 -0400

    Add some styling

--- client/src/settings/index.js
+++ client/src/settings/index.js
@@ -1,10 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+  Avatar,
+} from "@mui/material";
+import OfflineBoltIcon from "@mui/icons-material/OfflineBolt";
+import { styled } from "@mui/system";
 import { useAuth } from "../auth/AuthContext";
+import config from "../config.json";
 
-const config = require("../config.json");
+const StyledContainer = styled(Container)(() => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+}));
+
+const StyledForm = styled("form")(({ theme }) => ({
+  width: "100%",
+  marginTop: theme.spacing(1),
+}));
 
 function Settings() {
+  const theme = useTheme();
   const { currentUser, logout } = useAuth();
   const [errorMsg, setErrorMsg] = useState("");
   const [loading, setLoading] = useState(false);
@@ -90,53 +112,121 @@ function Settings() {
   };
 
   return (
-    <div className="settings">
-      <h1>Settings</h1>
-      {errorMsg && <div>{errorMsg}</div>}
+    <StyledContainer
+      maxWidth="md"
+      sx={{
+        marginTop: theme.spacing(2),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: theme.palette.background.paper,
+        borderRadius: theme.shape.borderRadius,
+        paddingTop: theme.spacing(6),
+        paddingBottom: theme.spacing(10),
+        paddingLeft: theme.spacing(6),
+        paddingRight: theme.spacing(6),
+        boxShadow: theme.shadows[3],
+      }}
+    >
+      <Typography
+        component="h1"
+        variant="h3"
+        color="primary"
+        sx={{ textAlign: "center" }}
+      >
+        JuiceFinder
+      </Typography>
+      <Avatar
+        sx={{
+          margin: theme.spacing(1),
+          color: theme.palette.secondary.main,
+          backgroundColor: theme.palette.success.main,
+        }}
+      >
+        <OfflineBoltIcon />
+      </Avatar>
       <div>
-        <h3>Email:</h3>
-        <p>{currentUser.email}</p>
-      </div>
-      <div>
-        <h3>Creation Time:</h3>
-        <p>{new Date(currentUser.metadata.creationTime).toLocaleString()}</p>
-      </div>
-      <div>
-        <h3>User ID:</h3>
-        <p>{currentUser.uid}</p>
+        {errorMsg && (
+          <Box>
+            <Typography color="error">{errorMsg}</Typography>
+          </Box>
+        )}
+        <Box sx={{ fontWeight: "Bold", marginTop: theme.spacing(3) }}>
+          <Typography variant="h4">Account Information</Typography>
+        </Box>
+        <Typography>
+          <b>Email:</b> {currentUser.email}
+        </Typography>
+        <Typography>
+          <b>User ID:</b> {currentUser.uid}
+        </Typography>
+
+        {vehicleInfo && (
+          <div style={{ marginTop: theme.spacing(3) }}>
+            <Box sx={{ fontWeight: "Bold" }}>
+              <Typography variant="h4">Saved Vehicle Information</Typography>
+            </Box>
+            <Typography>
+              <b>Vehicle ID:</b> {vehicleInfo.id}
+            </Typography>
+            <Typography>
+              <b>Brand:</b> {vehicleInfo.brand}
+            </Typography>
+            <Typography>
+              <b>Type:</b> {vehicleInfo.vehicle_type}
+            </Typography>
+            <Typography>
+              <b>Model:</b> {vehicleInfo.model}
+            </Typography>
+            <Typography>
+              <b>Release Year:</b> {vehicleInfo.release_year}
+            </Typography>
+            <Typography>
+              <b>Variant:</b> {vehicleInfo.variant}
+            </Typography>
+            <Typography>
+              <b>Vehicle Type:</b> {vehicleInfo.type}
+            </Typography>
+            <Typography>
+              <b>Usable Battery Size:</b> {vehicleInfo.usable_battery_size}
+            </Typography>
+            <Typography>
+              <b>Energy Consumption:</b> {vehicleInfo.energy_consumption}
+            </Typography>
+          </div>
+        )}
       </div>
 
-      {vehicleInfo && (
-        <div>
-          <h3>Vehicle Information:</h3>
-          <p>ID: {vehicleInfo.id}</p>
-          <p>Brand: {vehicleInfo.brand}</p>
-          <p>Type: {vehicleInfo.vehicle_type}</p>
-          <p>Model: {vehicleInfo.model}</p>
-          <p>Release Year: {vehicleInfo.release_year}</p>
-          <p>Variant: {vehicleInfo.variant}</p>
-          <p>Vehicle Type: {vehicleInfo.type}</p>
-          <p>Usable Battery Size: {vehicleInfo.usable_battery_size}</p>
-          <p>Energy Consumption: {vehicleInfo.energy_consumption}</p>
-        </div>
-      )}
+      <Box>
+        <Typography variant="h6">
+          Add/Update Vehicle ID (for testing):
+        </Typography>
+        <StyledForm>
+          <TextField
+            fullWidth
+            value={vehicleId}
+            placeholder="Type in vehicle id"
+            onChange={(e) => setVehicleId(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleUpdateVehicle}
+          >
+            Update Vehicle
+          </Button>
+        </StyledForm>
+      </Box>
 
-      <button type="button" disabled={loading} onClick={handleLogout}>
+      <Button
+        variant="contained"
+        color="primary"
+        disabled={loading}
+        onClick={handleLogout}
+      >
         Logout (for testing)
-      </button>
-      <div>
-        <h3>Add/Update Vehicle ID (for testing):</h3>
-        <input
-          type="text"
-          value={vehicleId}
-          placeholder="Type in vehicle id"
-          onChange={(e) => setVehicleId(e.target.value)}
-        />
-        <button type="button" onClick={handleUpdateVehicle}>
-          Update Vehicle
-        </button>
-      </div>
-    </div>
+      </Button>
+    </StyledContainer>
   );
 }
 
