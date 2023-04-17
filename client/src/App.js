6097250524eb218a911753c2ec85e616e0415df0
lizyn <26119970+lizyn@users.noreply.github.com>
2023-04-17 02:43:13 -0400

    Display map, searchbar and marker with Mapbox

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,11 @@ import WithAuthRedirect from "./common/WithAuthRedirect";
 import Landing from "./landingPage";
 import Login from "./auth/Login";
 import Signup from "./auth/Signup";
-import Map from "./map";
+import HomePage from "./home";
 import Statistics from "./statistics";
 import Settings from "./settings";
 
 function App() {
-  // console.log("trigger client tests. Can safely delete this line later.");
   return (
     <Router>
       <AuthProvider>
@@ -18,7 +17,7 @@ function App() {
           <Route exact path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/map" element={<Map />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/statistics" element={<Statistics />} />
           <Route
             path="/settings"
---
+++ client/src/home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Map from "./Map";
+
+export default function HomePage() {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Map />
+    </div>
+  );
+}
