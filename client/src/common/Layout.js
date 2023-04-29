8e46ced0cf43187aa4ef8569e438e9a73a94853a
YichengShen <jsszsyc@hotmail.com>
2023-04-29 11:24:13 -0400

    Update layout to support displaying footer on all pages

--- client/src/common/Layout.js
+++ client/src/common/Layout.js
@@ -1,14 +1,40 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import PropTypes from "prop-types";
+import MapGL from "react-map-gl";
 import Footer from "./Footer";
 import ResponsiveAppBar from "./AppBar";
 
 function Layout({ children }) {
+  const hasMapGL = React.Children.toArray(children).some(
+    (child) => child.type === MapGL
+  );
+
+  const getSx1 = () => {
+    if (hasMapGL) {
+      return { display: "flex", flexDirection: "column", height: "100vh" };
+    }
+    return {
+      display: "flex",
+      flexDirection: "column",
+      minHeight: "100vh",
+    };
+  };
+
+  const getSx2 = () => {
+    if (hasMapGL) {
+      return { height: "calc(100% - 15px)", flexGrow: 1 };
+    }
+    return {
+      minHeight: "calc(100% - 15px)",
+      flexGrow: 1,
+    };
+  };
+
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+    <Box sx={getSx1()}>
       <ResponsiveAppBar />
-      <Box sx={{ height: "calc(100% - 15px)", flexGrow: 1 }}>{children}</Box>
+      <Box sx={getSx2()}>{children}</Box>
       <Footer />
     </Box>
   );
